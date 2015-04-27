d384ccd8fa8603622538ee9de838ccd1051feaff
Arsalan Khan Ahmed <arsalankhanahmed@gmail.com>
2015-04-27 12:37:48 +0500

    Code Hunt Solution
    Code Hunt Solution

---
+++ CodeHuntSolution/CodeHuntLevel-3.7/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CodeHuntLevel_3._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnter = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEnter
+            // 
+            this.txtEnter.Location = new System.Drawing.Point(1, 0);
+            this.txtEnter.Name = "txtEnter";
+            this.txtEnter.Size = new System.Drawing.Size(283, 20);
+            this.txtEnter.TabIndex = 0;
+            this.txtEnter.TextChanged += new System.EventHandler(this.txtEnter_TextChanged);
+            this.txtEnter.Leave += new System.EventHandler(this.txtEnter_Leave);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(1, 26);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(283, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtEnter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEnter;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ CodeHuntSolution/CodeHuntLevel-3.7/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodeHuntLevel_3._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Puzzle(string s)
+        {
+            int m = 5;
+            Math.Pow(m, 2);
+            
+            char[] c = new char[s.Length];
+            for (int i = 0; i < s.Length; i++)
+                c[i] = (char)((s[i] + m - (int)'a') % 26 + (int)'a');
+            return new String(c);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtEnter_TextChanged(object sender, EventArgs e)
+        {
+            //txtResult.Text = Puzzle(txtEnter.Text);
+            //bool x = false;
+            //bool y = true;
+            //bool res = x || y ? true : false;
+            //txtResult.Text = (x || y ? true : false).ToString();//.ToString();
+        }
+
+        private void txtEnter_Leave(object sender, EventArgs e)
+        {
+            string s = txtEnter.Text;
+            //string str = "";
+            //for(int i = 0 ; i < s.Length; i++)
+            //{
+            //   if (i==s.Length-1 || s[i+1].Equals(' '))
+            //        str += Char.ToUpper(s[i]);
+            //   else
+            //       str += s[i];
+            //}
+            //txtResult.Text = str;
+            //int x = 0;
+            //string str = "";
+            //string vowels = "aeiou";
+            //str = s.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+            //str = new string(s.Where(c => !vowels.Contains(c)).ToArray());
+            //bool bol = s.Contains('r');
+            //txtResult.Text = str;
+            //str = s.Substring((s.Length / 2)+1).ToUpper();
+            //str += s.Substring(s.Length/2);
+            //char[] charArray = s.ToCharArray();
+            //Array.Reverse(charArray);
+            //return s + new string(charArray);
+            //Array.Reverse(charArray);
+            //return s + new string(charArray);
+            //txtResult.Text = s + new string(charArray); // s.Substring(s.Length - 3, 3);
+            //txtResult.Text = s[x].ToString();
+            string a, b, c = "";
+            a = "a";
+            b = "r";
+            c = "l";
+            bool IF = s.Contains(a);// + b + c);
+            //repeated String 
+            b = string.Join(" ", Enumerable.Repeat('a', 2));
+            string output = "";
+            int space = 1;
+            for (char start = '`'; start < 'z'; start++) 
+            {
+                //start = (char)96;
+                //output += start;
+                output += (char)(start + space);
+                output += new String(' ',1);
+            }
+            txtResult.Text = output.TrimEnd();
+
+            //factorile
+            //int res = 0;
+            //int lowerLimit = 2;
+            //int upperLimit = 3;
+            //for (int o = upperLimit; o >= lowerLimit; o--)
+            //{
+            //    res += Factorial(o, lowerLimit);
+            //}
+            int n = 2;
+            string str = "";
+            for (int i = 0; i <= n; i++)
+            {
+                str += string.Join(" ",Enumerable.Repeat('#', i));
+            }
+            txtResult.Text =  str;  
+        }
+
+        int Factorial(int u, int l) 
+        {
+            int res = 1;
+            for (int it = l; it <= u; it++)
+                res = res * it;
+            return res;
+        }
+    }
+}
+
